a530323313797c190f0d65937a6f737ffd3ea3d9
Melody Plan <melodyplan@gmail.com>
2017-05-20 13:51:21 -0700

    main image works

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const API_KEY = 'GHtmvSXNIA94h6fHzpAR8qxHQsNYydkHDcXluDQr'
 const ROOT_URL = 'https://api.nasa.gov/planetary/apod'
 
-$('.open-btn').on('click', function() {
-  $('aside').addClass('open');
-})
 
-$('.close-btn').on('click', function() {
-  $('aside').removeClass('open');
-})
+function main() {
 
-//comment
+}
+
+function fetchMainImage() {
+  axios.get(ROOT_URL, { params: { api_key: API_KEY } })
+    .then(function(response) {
+      console.log(response.data);
+      $('.main-image').attr('style', `background-image: url(${response.data.url}`);
+    })
+}
+
+function eventHandlers() {
+  $('.open-btn').on('click', function() {
+    $('aside').addClass('open');
+  })
+
+  $('.close-btn').on('click', function() {
+    $('aside').removeClass('open');
+  })
+}
+
+$(document).ready(function() {
+  eventHandlers();
+  fetchMainImage();
+  main();
+})

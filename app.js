051b628aa8de6596c4e78e0f398a7fc27bee5e03
Melody Plan <melodyplan@gmail.com>
2017-05-23 22:39:34 -0700

    extended fetchMainImage and added updateMonth and buildCalendar

--- app.js
+++ app.js
@@ -3,15 +3,37 @@ const ROOT_URL = 'https://api.nasa.gov/planetary/apod'
 
 
 function main() {
-
+  //extend the fetchMainImage function to be able to take a date as one of its arguments and fetch the correct date image
 }
 
-function fetchMainImage() {
-  axios.get(ROOT_URL, { params: { api_key: API_KEY } })
-    .then(function(response) {
-      console.log(response.data);
-      $('.main-image').attr('style', `background-image: url(${response.data.url}`);
+function fetchMainImage(date) {
+  axios.get(ROOT_URL, {
+      params: {
+        api_key: API_KEY,
+        date: date //as long as you change the paramater by yyyy-mm-dd and pass it through fetchMainImage as a parameter it should be good to go
+      }
     })
+    .then(function(response) {
+      if (response.data.media_type === 'image') {
+        $('.main-image').attr('style', `background-image: url(${response.data.url}`);
+        $('.main-image iframe').addClass('hidden');
+      } else if (response.data.media_type === 'video') {
+        $('.main-image iframe').attr('src', response.data.url);
+        $('.main-image iframe').removeClass('hidden');
+      }
+    })
+}
+
+function updateMonth() {
+  $('.current-month').text(moment().format('MMM'))
+}
+
+function buildCalendar() {
+  const range = moment.range('2017-05-01', '2017-05-31');
+
+  for (let month of range.by('days')) {
+    console.log(month.format('dd'));
+  }
 }
 
 function eventHandlers() {
@@ -27,5 +49,7 @@ function eventHandlers() {
 $(document).ready(function() {
   eventHandlers();
   fetchMainImage();
+  updateMonth();
+  buildCalendar();
   main();
 })

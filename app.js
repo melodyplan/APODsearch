81e22925d9f3669e02d652b930989d262d8a32b4
Melody Plan <melodyplan@gmail.com>
2017-06-09 19:15:51 -0700

    DONE

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wedesday', 'Thursday', 'Friday', 'Saturday']
 let state = {
-  month: moment().month(),
+  month: moment().month() + 1,
   year: moment().year()
+}
+console.log(state)
+var youtube_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(query) {
+  axios.get(youtube_URL, {
+    params: {
+      part: 'snippet',
+      key: 'AIzaSyABEG1EiZyTt2kxzYWbcU2Nhka23N538Sg',
+      q: query
+    }
+  })
 }
 
 function fetchMainImage(date) {
@@ -27,11 +39,11 @@ function fetchMainImage(date) {
     })
 }
 
-function fetchAllImages(month, year) {
+function fetchAllImages() {
   let $el
   const promises = $('.day').map(function(index) {
     $el = $(this)
-    let date = `${year}-${month + 1}-${$el.text()}`
+    let date = `${state.year}-${state.month}-${$el.text()}`
 
     if (!$el.text().trim()) { return false }
     if ((moment().unix() - moment(date, 'YYYY-M-D').unix() < 0 )) { return false }
@@ -48,17 +60,43 @@ function fetchAllImages(month, year) {
       state.results = results
       $('.day').each(function(index) {
         if (!results[index]) { return };
-        $(this).attr('style', `background-image: url(${results[index].url}`);
+        if (results[index].media_type === 'image') {
+          $(this).attr('style', `background-image: url(${results[index].url}`)
+        } else if (results[index].media_type === 'video') {
+          console.log(results[index].url)
+          $(this).attr('style', `background-image: url('https://img.youtube.com/vi/${results[index].url.split('/')[4].split('?')[0]}/0.jpg'`)
+        }
       })
     })
 }
 
 function updateMonth(date) {
-  if (date) {
-    return $('.current-month').text(moment(date).format('MMM'))
-  }
-  return $('.current-month').text(moment().format('MMM'))
+  return $('.current-month').text(moment(`${state.year}-${state.month}-1`, 'YYYY-M-D').format('MMM'))
 }
+
+function nextMonth(date) {
+  state.month ++
+  buildCalendar()
+}
+
+function prevMonth(date) {
+  state.month --
+  buildCalendar()
+}
+
+
+function updateYear(date) {
+  if (date) {
+    return $('.current-year').text(moment(date).format('YYYY'))
+  }
+  return $('.current-year').text(moment().format('YYYY'))
+}
+
+// function previousMonth(date) {
+//   if (date) {
+//     return $('.current-month').moment().subtract(1, 'months').format('MMM')
+//   }
+// }
 
 function reflect(promise) {
   if (!promise) {
@@ -76,11 +114,10 @@ function reflect(promise) {
 }
 
 function buildCalendar() {
-  const month = state.month
   const year = state.year
-  const firstOfMonth = moment(`${year}-${month + 1}-1`, 'YYYY-M-D').day();
+  const firstOfMonth = moment(`${state.year}-${state.month}-1`, 'YYYY-M-D').day();
   let calendar = [];
-  for (let i = 0; i < daysInMonth[month] + firstOfMonth; i++) {
+  for (let i = 0; i < daysInMonth[state.month] + firstOfMonth; i++) {
     if (i < firstOfMonth) {
       calendar.push('')
     } else {
@@ -94,9 +131,10 @@ function buildCalendar() {
     calendarHtml.push('<div class="day"> </div>')
   }
   $('.week').html(calendarHtml)
-  fetchAllImages(month, year)
+  fetchAllImages()
   fetchMainImage()
   updateMonth()
+  updateYear()
 }
 
 function eventHandlers() {
@@ -109,12 +147,12 @@ function eventHandlers() {
   })
 
   $('.back-btn').on('click', function() {
-    state.month = state.month - 1
+    prevMonth()
     buildCalendar()
   })
 
   $('.forward-btn').on('click', function() {
-    state.month = state.month + 1
+    nextMonth()
     buildCalendar()
   })
 }
@@ -124,7 +162,11 @@ function thumbnailClickHandler() {
     const index = $(this).index()
     const data = state.results[index]
     if (!data) { return }
-    $('.modal-image').attr('style', `background-image: url(${data.hdurl}`)
+    if (data.media_type === 'image') {
+      $('.modal-image').attr('style', `background-image: url(${data.hdurl}`).html('')
+    } else if (data.media_type === 'video') {
+      $('.modal-image').html(`<iframe width="960" height="540" src="${data.url}" frameborder="0" allowfullscreen></iframe>`)
+    }
     $('.modal-explanation').text(data.explanation)
     $('.melody-overlay').removeClass('hidden')
   })
